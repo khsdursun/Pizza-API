8c175cae78a218135290d9bdc3f73f93139d7e9d
khsdursun <khasanovdursun018@gmail.com>
2025-11-21 13:43:51 +0500

    добавил список из 11 ресторанов (пока без SQL)

--- main.py
+++ main.py
@@ -28,3 +28,50 @@ class Review(BaseModel):
     rating: int
     text: str
     
+    
+restaurants = [
+    Restaurant(
+        name="La Piazza",
+        address="ул. Панфилова, 12, Алматы"
+    ),
+    Restaurant(
+        name="Sushi Master",
+        address="пр. Абая, 45, Алматы"
+    ),
+    Restaurant(
+        name="Burger House",
+        address="ул. Сейфуллина, 221, Алматы"
+    ),
+    Restaurant(
+        name="Tokyo Grill",
+        address="ул. Байтурсынова, 89, Алматы"
+    ),
+    Restaurant(
+        name="Steak & Grill",
+        address="пр. Назарбаева, 32, Алматы"
+    ),
+    Restaurant(
+        name="Khachapuri & Wine",
+        address="ул. Толе би, 58, Алматы"
+    ),
+    Restaurant(
+        name="Beshbarmak House",
+        address="ул. Достык, 99, Алматы"
+    ),
+    Restaurant(
+        name="Casa Mexicana",
+        address="пр. Абылай хана, 12, Алматы"
+    ),
+    Restaurant(
+        name="French Corner",
+        address="ул. Жибек жолы, 17, Алматы"
+    ),
+    Restaurant(
+        name="Tandoor Palace",
+        address="ул. Гагарина, 130, Алматы"
+    ),
+    Restaurant(
+        name="Pizza Familia",
+        address="ул. Сатпаева, 77, Алматы"
+    ),
+]
