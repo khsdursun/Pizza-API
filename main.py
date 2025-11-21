33a49c9d74ba61da66d9020d9f6ebede9a95ed69
khsdursun <khasanovdursun018@gmail.com>
2025-11-21 11:08:01 +0500

    Добавил необходимые модели данных

--- main.py
+++ main.py
@@ -6,3 +6,25 @@ from pydantic import BaseModel
 app = FastAPI()
 
 
+class Restaurant(BaseModel):
+    name: str
+    address: str
+    
+class Chef(BaseModel):
+    name: str
+    restaurant_id = int
+    
+class Pizza(BaseModel):
+    name: str
+    cheese: str
+    dough: str
+    secret_ingredient = str
+
+class Ingredient(BaseModel):
+    name: List[str]
+    
+class Review(BaseModel):
+    restaurant: str
+    rating: int
+    text: str
+    
